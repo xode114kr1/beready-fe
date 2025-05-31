22c32393c1076ca3284a733a87d7044ef3d38e75
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-05-31 17:15:01 +0900

    feat : 대기시간 스크린 UI 구현

--- screens/WaitEstimationScreen/WaitEstimationScreen.js
+++ screens/WaitEstimationScreen/WaitEstimationScreen.js
@@ -1,10 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import GradientScreenWrapper from "../../components/GradientScreenWrapper";
+import { LinearGradient } from "expo-linear-gradient";
+
+const { width } = Dimensions.get("window");
+const CIRCLE_SIZE = width * 0.85;
 
 export default function WaitEstimationScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>⏳ Wait Estimation Screen</Text>
-    </View>
+    <GradientScreenWrapper>
+      <View style={styles.container}>
+        <LinearGradient
+          colors={["#A7D8FF", "#F5FAFF"]}
+          style={styles.waiting_box}
+        >
+          <Text style={styles.time}>04:03</Text>
+          <Text style={styles.people}>
+            대기인원 : <Text style={styles.peopleStrong}>05명</Text>
+          </Text>
+        </LinearGradient>
+      </View>
+    </GradientScreenWrapper>
   );
 }
 
@@ -16,5 +31,32 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  waiting_box: {
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE / 2,
+    justifyContent: "center",
+    alignItems: "center",
+
+    shadowColor: "#DFDFDF",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  time: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "#3B63C4",
+  },
+  people: {
+    marginTop: 8,
+    fontSize: 18,
+    color: "#333",
+  },
+  peopleStrong: {
+    color: "#3B63C4",
+    fontWeight: "bold",
   },
 });
