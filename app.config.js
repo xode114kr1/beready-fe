b591eb3c95b5443fa601dd39d36fc9d323456537
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-09-08 18:40:03 +0900

    feat: 배포 설정

--- app.config.js
+++ app.config.js
@@ -1,11 +1,38 @@
 import "dotenv/config";
 
-export default ({ config }) => {
-  return {
-    ...config,
-    extra: {
-      BACKEND_LOCAL_URL: process.env.BACKEND_LOCAL_URL,
-      FASTAPI_LOCAL_URL: process.env.FASTAPI_LOCAL_URL,
+export default ({ config }) => ({
+  ...config,
+  name: "beready-fe",
+  slug: "beready-fe",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: { supportsTablet: true },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-  };
-};
+    edgeToEdgeEnabled: true,
+  },
+  web: { favicon: "./assets/favicon.png" },
+
+  runtimeVersion: { policy: "sdkVersion" },
+  updates: {
+    url: "https://u.expo.dev/c05012a1-77e5-4f74-9fbf-825ca3800ba5", // projectId 기반
+  },
+  extra: {
+    BACKEND_LOCAL_URL:
+      process.env.BACKEND_LOCAL_URL ?? "http://192.168.0.138:5000/api/",
+    FASTAPI_LOCAL_URL:
+      process.env.FASTAPI_LOCAL_URL ?? "http://192.168.0.138:8000/api/",
+    eas: { projectId: "c05012a1-77e5-4f74-9fbf-825ca3800ba5" },
+  },
+});
