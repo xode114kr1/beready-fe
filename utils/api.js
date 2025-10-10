c09a666d1fe9dd80cfc33bb8b3c78ec0c67d3cab
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-11 02:25:54 +0900

    fix : api.js console.log 삭제

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Constants from "expo-constants";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const { EXPRESS_DEPLOY_URL, FASTAPI_LOCAL_URL } = Constants.expoConfig.extra;
+const { EXPRESS_DEPLOY_URL, FASTAPI_DEPLOY_URL } = Constants.expoConfig.extra;
 
 export const backApi = axios.create({
   baseURL: EXPRESS_DEPLOY_URL,
@@ -14,7 +14,6 @@ export const backApi = axios.create({
 backApi.interceptors.request.use(
   async (config) => {
     try {
-      console.log(EXPRESS_DEPLOY_URL);
       const token = await AsyncStorage.getItem("token");
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -28,7 +27,7 @@ backApi.interceptors.request.use(
 );
 
 export const fastApi = axios.create({
-  baseURL: FASTAPI_LOCAL_URL,
+  baseURL: FASTAPI_DEPLOY_URL,
   headers: {
     "Content-Type": "application/json",
   },
